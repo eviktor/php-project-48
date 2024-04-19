0f8fdf9d5f1bbfd579d4bd544fa794336827b89c
eviktor <vk7771199@gmail.com>
2024-04-19 21:50:37 +0700

    feat: Add Differ.php file

---
+++ src/Differ.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Differ\Differ;
+
+const FORMAT_STYLISH = 'stylish';
+
+function genDiff(string $firstData, string $secondData, string $format = FORMAT_STYLISH): string
+{
+    return '';
+}
