074a26ce2c0633cb9403bf5f242612ba2887fcc1
eviktor <vk7771199@gmail.com>
2024-05-02 19:31:56 +0700

    feat: Add yaml parser

--- src/Parsers/Yaml.php
+++ src/Parsers/Yaml.php
@@ -2,13 +2,27 @@
 
 namespace Differ\Parsers\Yaml;
 
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Exception\ParseException;
+
+use function Differ\Parsers\Json\parse as parseJson;
 use function Php\Immutable\Fs\Trees\trees\mkdir;
 use function Php\Immutable\Fs\Trees\trees\mkfile;
 
 /**
- * @return array<mixed>
+ * @return array<mixed>|false
  */
-function parse(string $content, string $rootName = ''): array
+function parse(string $content): array|false
 {
-    return mkdir($rootName);
+    try {
+        $data = Yaml::parse($content, Yaml::PARSE_OBJECT_FOR_MAP);
+    } catch (ParseException $exception) {
+        return false;
+    }
+    $emptyJson = '{}';
+    $json = is_null($data) ? $emptyJson : json_encode($data);
+    if ($json === false) {
+        return false;
+    }
+    return parseJson($json);
 }
